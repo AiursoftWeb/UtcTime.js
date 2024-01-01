83f28febc5d86da4223e5a1120473f70085940f9
AnduinXue <anduin@aiursoft.com>
2024-01-01 14:19:07 +0000

    Release 1.0.0 utc time.

---
+++ README.md
@@ -0,0 +1,41 @@
+# UtcTime.js
+
+[![MIT licensed](https://img.shields.io/badge/license-MIT-blue.svg)](https://gitlab.aiursoft.cn/aiursoft/utctime.js/-/blob/master/LICENSE) [![Pipeline stat](https://gitlab.aiursoft.cn/aiursoft/utctime.js/badges/master/pipeline.svg)](https://gitlab.aiursoft.cn/aiursoft/utctime.js/-/pipelines) [![ManHours](https://manhours.aiursoft.cn/r/gitlab.aiursoft.cn/aiursoft/utctime.js.svg)](https://gitlab.aiursoft.cn/aiursoft/utctime.js/-/commits/master?ref_type=heads) [![npm](https://img.shields.io/npm/v/@aiursoft/utctime.js?color=blue)](https://www.npmjs.com/package/@aiursoft/utctime.js) [![npm](https://img.shields.io/npm/dm/@aiursoft/utctime.js)](https://www.npmjs.com/package/@aiursoft/utctime.js)
+
+`@aiursoft/utctime.js` is a JavaScript library which converts UTC time to local time easily.
+
+## Installation
+
+Run the following command to install the most recent version of `@aiursoft/utctime.js` from npm:
+
+```sh
+npm install @aiursoft/utctime.js
+```
+
+## Importing
+
+## Reference JavaScript
+
+You also need to reference bootstrap's JavaScript file:
+
+```html
+<script type="module">
+  import UtcTime from './node_modules/@aiursoft/utctime.js/dist/esm/utctime.js'
+  new UtcTime({})
+</script>
+```
+
+## How to build locally
+
+```bash
+npm install
+npm run build
+```
+
+## How to contribute
+
+There are many ways to contribute to the project: logging bugs, submitting pull requests, reporting issues, and creating suggestions.
+
+Even if you with push rights on the repository, you should create a personal fork and create feature branches there when you need them. This keeps the main repository clean and your workflow cruft out of sight.
+
+We're also interested in your feedback on the future of this project. You can submit a suggestion or feature request through the issue tracker. To make this process more effective, we're asking that these include more information to help define them more clearly.
---
+++ src/utctime.ts
@@ -0,0 +1,105 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+interface UtcTimeSettings {
+  attr: string
+  daysAgo: string
+  hoursAgo: string
+  minutesAgo: string
+  secondsAgo: string
+  disableAgo: boolean
+  onSet: (obj: Element) => void
+  disableAutoUpdate: boolean
+}
+
+class UtcTime {
+
+  defaultSettings: UtcTimeSettings = {
+    attr: 'data-utc-time',
+    daysAgo: ' days ago',
+    hoursAgo: ' hours ago',
+    minutesAgo: ' minutes ago',
+    secondsAgo: ' seconds ago',
+    disableAgo: false,
+    onSet: function (obj: Element) {},
+    disableAutoUpdate: false,
+  }
+
+  constructor(settings: UtcTimeSettings) {
+    Object.assign(this.defaultSettings, settings)
+    const loop = () => {
+      this.initTime(this.defaultSettings)
+      if (!this.defaultSettings.disableAutoUpdate) {
+        setTimeout(loop, 1000)
+      }
+    }
+    loop()
+  }
+
+  timeSince(date: Date, settings: typeof this.defaultSettings): string {
+    const seconds = Math.floor((new Date().getTime() - date.getTime()) / 1000)
+    let interval = Math.floor(seconds / 2592000)
+    if (interval >= 1) {
+      return date.toLocaleDateString()
+    }
+    interval = Math.floor(seconds / 86400)
+    if (interval >= 1) {
+      return interval + settings.daysAgo
+    }
+    interval = Math.floor(seconds / 3600)
+    if (interval >= 1) {
+      return interval + settings.hoursAgo
+    }
+    interval = Math.floor(seconds / 60)
+    if (interval >= 1) {
+      return interval + settings.minutesAgo
+    }
+    if (interval >= 0) {
+      return Math.floor(seconds) + settings.secondsAgo
+    } else {
+      return date.toLocaleDateString()
+    }
+  }
+
+  getDate(inputString: string): Date {
+    if (inputString.endsWith(' UTC') || inputString.endsWith('Z')) {
+      return new Date(inputString)
+    }
+    if (!inputString.endsWith(' UTC')) {
+      const timeValue = inputString + ' UTC'
+      const date = new Date(timeValue)
+      if (!isNaN(date.getTime())) {
+        return date
+      }
+    }
+    if (!inputString.endsWith('Z')) {
+      const timeValue = inputString + 'Z'
+      const date = new Date(timeValue)
+      if (!isNaN(date.getTime())) {
+        return date
+      }
+    }
+    console.warn(
+      `jquery-utc-time: Input string ${inputString} can not be converted to UTC time and try to parse as typical date string.`,
+    )
+    return new Date(inputString)
+  }
+
+  initTime(settings: typeof this.defaultSettings): void {
+    document
+      .querySelectorAll('*[' + settings.attr + ']')
+      .forEach((timeField) => {
+        const sourceValue = timeField.getAttribute(settings.attr)
+        const date = this.getDate(sourceValue!)
+        let text = this.timeSince(date, settings)
+        if (settings.disableAgo) {
+          text = date.toLocaleDateString()
+        }
+        timeField.innerHTML = text
+        timeField.setAttribute('data-toggle', 'tooltip')
+        timeField.setAttribute('data-trigger', 'hover')
+        timeField.setAttribute('data-title', date.toLocaleString())
+        settings.onSet(timeField)
+      })
+  }
+}
+
+export default UtcTime
